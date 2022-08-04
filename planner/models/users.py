2bc4286452622dc92a26edb8fdb139c8719a80cc
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-04 10:48:47 +0430

    Defining User model

--- planner/models/users.py
+++ planner/models/users.py
@@ -1,3 +1,22 @@
 """
     This file will contain the model definition for user operations.
 """
+
+from pydantic import BaseModel, EmailStr
+from typing import Optional, List
+from planner.models.events import Event
+
+
+class User(BaseModel):
+    email: EmailStr
+    password: str
+    events: Optional[List[Event]]
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "email": "your_email@test.com",
+                "username": "your_username",
+                "events": [],
+            }
+        }
