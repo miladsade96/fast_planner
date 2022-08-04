691cd188ad50eccadc39a6eec10fa0e925363f3c
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-04 10:39:36 +0430

    Defining Event model

--- planner/models/events.py
+++ planner/models/events.py
@@ -4,3 +4,26 @@
 
 from pydantic import BaseModel
 from typing import List
+
+
+class Event(BaseModel):
+    """
+    This class will contain the model definition for events operations.
+    """
+    id: int
+    title: str
+    image: str
+    description: str
+    tags: List[str]
+    location: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "title": "Event Title",
+                "image": "https://image.com/image.jpg",
+                "description": "Event Description",
+                "tags": ["tag1", "tag2"],
+                "location": "Event Location"
+            }
+        }
