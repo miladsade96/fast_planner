ac16d355e7dd7b684422aa4e33d4ba6a825958aa
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-04 12:28:45 +0430

    Adding event router to main.py

--- planner/main.py
+++ planner/main.py
@@ -4,11 +4,13 @@
 
 from fastapi import FastAPI
 from planner.routes.users import user_router
+from planner.routes.events import events_router
 import uvicorn
 
 app = FastAPI()
 
 app.include_router(user_router, prefix="/user")
+app.include_router(events_router, prefix="/event")
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
