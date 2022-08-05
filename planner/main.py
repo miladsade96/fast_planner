03dc6d0f6e743160c896d681686fadbb0128e429
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-05 14:27:01 +0430

    Defining database connector and home route

--- planner/main.py
+++ planner/main.py
@@ -15,5 +15,16 @@ app = FastAPI()
 app.include_router(user_router, prefix="/user")
 app.include_router(events_router, prefix="/event")
 
+
+@app.on_event("startup")
+def on_startup():
+    conn()
+
+
+@app.get("/")
+async def home():
+    return RedirectResponse(url="/event/")
+
+
 if __name__ == '__main__':
     uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
