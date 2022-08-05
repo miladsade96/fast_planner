c72af4b4779b4f4784b903a4432a861887280a77
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-05 13:58:52 +0430

    Defining Event model as a sqlmodel

--- planner/models/events.py
+++ planner/models/events.py
@@ -2,27 +2,24 @@
     This file will contain the model definition for events operations.
 """
 
-from pydantic import BaseModel
-from typing import List
+from typing import List, Optional
+from sqlmodel import SQLModel, Field, Column, JSON
 
 
-class Event(BaseModel):
-    """
-    This class will contain the model definition for events operations.
-    """
-    id: int
+class Event(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
     title: str
     image: str
     description: str
-    tags: List[str]
     location: str
+    tags: List[str] = Field(sa_column=Column(JSON))
 
     class Config:
+        arbitrary_types_allowed = True
         schema_extra = {
             "example": {
-                "id": 1,
                 "title": "Event Title",
-                "image": "https://image.com/image.jpg",
+                "image": "https://image.com/image.png",
                 "description": "Event Description",
                 "tags": ["tag1", "tag2"],
                 "location": "Event Location"
