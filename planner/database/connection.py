898b42de7f062405982c2dd90feed03b7d7f7f35
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-06 11:21:28 +0430

    Implementing database settings class

--- planner/database/connection.py
+++ planner/database/connection.py
@@ -6,3 +6,20 @@ from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
 from typing import Optional
 from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    """
+        This class will handle the configurations for the database.
+    """
+    DATA_BASE_URL: Optional[str] = None
+
+    async def initialize_database(self):
+        """
+            This method will initialize the database.
+        """
+        client = AsyncIOMotorClient(self.DATA_BASE_URL)
+        await init_beanie(database=client.get_default_database(), document_models=[])
+
+        class Config:
+            env_file = '.env'
