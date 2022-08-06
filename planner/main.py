90de6b1f30efef9087cbc0e732237faba6257de1
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-06 16:53:27 +0430

    Implementing init_db function and home route

--- planner/main.py
+++ planner/main.py
@@ -12,8 +12,22 @@ from planner.routes.users import user_router
 
 app = FastAPI()
 
+settings = Settings()
+
+# Register routes
 app.include_router(user_router, prefix="/user")
 app.include_router(events_router, prefix="/event")
+
+
+@app.on_event("startup")
+async def init_db():
+    await settings.initialize_database()
+
+
+@app.get("/")
+async def home():
+    return RedirectResponse(url="/event/")
+
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
