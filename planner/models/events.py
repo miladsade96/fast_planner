db8b33853a29cc6a09cc321f7023678ba2acbcd3
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-06 11:25:18 +0430

    Implementing EventUpdate model

--- planner/models/events.py
+++ planner/models/events.py
@@ -29,3 +29,25 @@ class Event(Document):
 
     class Settings:
         name = "events"
+
+
+class EventUpdate(Document):
+    """
+    This class will contain the model definition for events operations.
+    """
+    title: Optional[str]
+    image: Optional[str]
+    description: Optional[str]
+    tags: Optional[List[str]]
+    location: Optional[str]
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "title": "Event Title",
+                "image": "https://image.com/image.jpg",
+                "description": "Event Description",
+                "tags": ["tag1", "tag2"],
+                "location": "Event Location"
+            }
+        }
