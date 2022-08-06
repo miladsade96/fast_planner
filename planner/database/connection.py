baabda8c638141d45e61c093c1f3aa9365bddd05
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-06 11:16:31 +0430

    Importing relevant modules

--- planner/database/connection.py
+++ planner/database/connection.py
@@ -1,3 +1,8 @@
 """
     This file will handle the database abstractions and configurations.
 """
+
+from beanie import init_beanie
+from motor.motor_asyncio import AsyncIOMotorClient
+from typing import Optional
+from pydantic import BaseSettings
