a0aec6acdcf5d0fa7fbc978acd4236caef3e1b5b
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-06 11:27:07 +0430

    Importing relevant modules

--- planner/models/users.py
+++ planner/models/users.py
@@ -2,7 +2,11 @@
     This file will contain the model definition for user operations.
 """
 
+from typing import List, Optional
+from beanie import Document, Link
 from pydantic import BaseModel, EmailStr
+from planner.models.events import Event
+
 
 
 class User(BaseModel):
