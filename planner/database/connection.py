0b6ed54c6a67625a3b2cb6e211cb832944f4ba99
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-06 11:31:20 +0430

    Updating document models

--- planner/database/connection.py
+++ planner/database/connection.py
@@ -6,6 +6,8 @@ from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
 from typing import Optional
 from pydantic import BaseSettings
+from planner.models.users import User
+from planner.models.events import Event
 
 
 class Settings(BaseSettings):
@@ -19,7 +21,7 @@ class Settings(BaseSettings):
             This method will initialize the database.
         """
         client = AsyncIOMotorClient(self.DATA_BASE_URL)
-        await init_beanie(database=client.get_default_database(), document_models=[])
+        await init_beanie(database=client.get_default_database(), document_models=[Event, User])
 
         class Config:
             env_file = '.env'
