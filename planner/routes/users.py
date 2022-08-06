392e44db159dc1dd3024545287627aca0d3d11a6
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-06 12:25:51 +0430

    Refactoring sign_user_in route

--- planner/routes/users.py
+++ planner/routes/users.py
@@ -25,17 +25,11 @@ async def sign_user_up(user: User) -> dict:
 
 @user_router.post("/signin")
 async def sign_user_in(user: UserSignIn) -> dict:
-    if user.email not in users:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="User does not exist"
-        )
-
-    if users[user.email].password != user.password:
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail="Wrong credential passed"
-        )
-    return {
-        "message": "User signed in successfully"
-    }
+    user_exist = await User.find_one(User.email == user.email)
+    if not user_exist:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    else:
+        if user.password == user_exist.password:
+            return {"message": "User signed in successfully"}
+        else:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid details passed.")
