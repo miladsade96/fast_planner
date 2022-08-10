6460a6eda3bce9296c50e027a5c3b1fd01084cdf
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-10 12:19:27 +0430

    Defining password context

--- planner/auth/hash_password.py
+++ planner/auth/hash_password.py
@@ -2,3 +2,8 @@
     This module is used to hash a password.
     Author: Milad sadeghi DM - EverLookNeverSee@GitHub
 """
+
+from passlib.context import CryptContext
+
+# Password context
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
