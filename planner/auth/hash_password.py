7353b23d6e4ff5aea05f1257ed5d97523fe9cf8c
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-10 12:19:49 +0430

    Implementing Hash_Password class

--- planner/auth/hash_password.py
+++ planner/auth/hash_password.py
@@ -7,3 +7,27 @@ from passlib.context import CryptContext
 
 # Password context
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+class HashPassword:
+    """
+        This class is used to hash a password and check if the password is correct.
+    """
+    @staticmethod
+    def create_hash(password: str) -> str:
+        """
+        This method is used to hash a password
+        :param password: password to be hashed
+        :return: hashed password
+        """
+        return pwd_context.hash(password)
+
+    @staticmethod
+    def verify_hash(plain_password: str, hashed_password: str) -> bool:
+        """
+        This method is used to verify if the password is correct
+        :param plain_password: password to be verified
+        :param hashed_password: hashed password
+        :return: True if the password is correct, False otherwise
+        """
+        return pwd_context.verify(plain_password, hashed_password)
