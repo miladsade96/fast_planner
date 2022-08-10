780c344bb26b6dc25b15dfcf0d631589d9722ef7
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-10 13:03:35 +0430

    Importing relevant modules

--- planner/auth/authenticate.py
+++ planner/auth/authenticate.py
@@ -2,3 +2,7 @@
     This module handles the authentication of the user.
     Author: Milad Sadeghi DM - EverLookNeverSee@GitHub
 """
+
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import HTTPException, Depends, status
+from planner.auth.jwt_handler import verify_access_token
