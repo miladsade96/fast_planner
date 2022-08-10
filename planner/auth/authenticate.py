922c570b57772596849c759a7d961d9eab9ff266
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-10 13:09:20 +0430

    Implementing authenticate section

--- planner/auth/authenticate.py
+++ planner/auth/authenticate.py
@@ -6,3 +6,18 @@
 from fastapi.security import OAuth2PasswordBearer
 from fastapi import HTTPException, Depends, status
 from planner.auth.jwt_handler import verify_access_token
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/users/signin")
+
+
+async def authenticate(token: str = Depends(oauth2_scheme)) -> str:
+    """
+    This function authenticates the user.
+    :param token: The token of the user.
+    :return: The token of the user.
+    """
+    if not token:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Signin for access")
+    decoded_token = verify_access_token(token)
+    return decoded_token["user"]
