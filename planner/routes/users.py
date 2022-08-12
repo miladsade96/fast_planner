bda485d67c784eaa8d57ed305ced2d03a33ca138
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-12 13:52:46 +0430

    Refactoring signin route

--- planner/routes/users.py
+++ planner/routes/users.py
@@ -2,10 +2,12 @@
     This file will handle routing operations such as the registration and signing-in of users.
 """
 
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, status, Depends
 from planner.database.connection import Database
-from planner.models.users import UserSignIn, User
+from planner.models.users import User, TokenResponse
 from planner.auth.hash_password import HashPassword
+from fastapi.security import OAuth2PasswordRequestForm
+from planner.auth.jwt_handler import create_access_token
 
 
 # Defining a router for the users endpoint
@@ -28,13 +30,14 @@ async def sign_user_up(user: User) -> dict:
         return {"message": "User created successfully"}
 
 
-@user_router.post("/signin")
-async def sign_user_in(user: UserSignIn) -> dict:
-    user_exist = await User.find_one(User.email == user.email)
-    if not user_exist:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
-    else:
-        if user.password == user_exist.password:
-            return {"message": "User signed in successfully"}
+@user_router.post("/signin", response_model=TokenResponse)
+async def sign_user_in(user: OAuth2PasswordRequestForm = Depends()) -> dict:
+    user_exist = await User.find_one(User.email == user.username)
+    if user_exist:
+        if hash_password.verify_hash(user.password, user_exist.password):
+            access_token = create_access_token(user_exist.email)
+            return {"access_token": access_token, "token_type": "Bearer"}
         else:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid details passed.")
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect password")
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
