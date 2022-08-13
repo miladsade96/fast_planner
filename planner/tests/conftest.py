22bb44150e909d17f82003930fef01dbbf04ec27
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-13 19:49:52 +0430

    Importing relevant modules

--- planner/tests/conftest.py
+++ planner/tests/conftest.py
@@ -2,3 +2,11 @@
     This file will be responsible for creating an instance of our application required by the test files.
     Author: Milad Sadeghi DM - EverLookNeverSee@GitHub
 """
+
+import httpx
+import pytest
+import asyncio
+from planner.main import app
+from planner.models.users import User
+from planner.models.events import Event
+from planner.database.connection import Settings
