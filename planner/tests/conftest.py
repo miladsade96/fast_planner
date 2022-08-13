2d62de40283ea3ce4eba0c70f1810f6e179b3a8d
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-13 20:00:29 +0430

    Defining default_client fixture

--- planner/tests/conftest.py
+++ planner/tests/conftest.py
@@ -25,3 +25,13 @@ async def init_db():
     test_settings = Settings()
     test_settings.DATABASE_URL = "mongodb://localhost:27017/testdb"
     await test_settings.initialize_database()
+
+
+@pytest.fixture(scope="session")
+async def default_client():
+    await init_db()
+    async with httpx.AsyncClient(app=app, base_url="http://app") as client:
+        yield client
+        # clean up resources
+        await Event.find_all().delete()
+        await User.find_all().delete()
