7bcdd480e62b3e13d02f7f4d0ebe76c4b7e8a91f
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-15 09:22:16 +0430

    Implementing test_sign_new_user

--- planner/tests/test_login.py
+++ planner/tests/test_login.py
@@ -2,3 +2,25 @@
     Test Signup and signin routes
     Author: Milad Sadeghi.DM - EverLookNeverSee@GitHub
 """
+
+import httpx
+import pytest
+
+
+@pytest.mark.asyncio
+async def test_sign_new_user(default_client: httpx.AsyncClient) -> None:
+    payload = {
+        "email": "testuser@test.com",
+        "password": "testpassword",
+        "events": []
+    }
+    headers = {
+        "accept": "application/json",
+        "Content-type": "application/json"
+    }
+    test_response = {
+        "message": "User created successfully"
+    }
+    response = await default_client.post("/user/signup", json=payload, headers=headers)
+    assert response.status_code == 200
+    assert response.json() == test_response
