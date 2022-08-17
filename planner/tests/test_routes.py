9ef81c3b69dc23dc7e43e2bf4d5128598b4cdfa9
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-17 13:23:46 +0430

    Implementing access_token fixture

--- planner/tests/test_routes.py
+++ planner/tests/test_routes.py
@@ -7,3 +7,12 @@ import httpx
 import pytest
 from planner.models.events import Event
 from planner.auth.jwt_handler import create_access_token
+
+
+@pytest.fixture(scope="module")
+async def access_token():
+    """
+    This fixture is used to create an access token for the tests.
+    :return: access token
+    """
+    return create_access_token("testuser@test.com")
