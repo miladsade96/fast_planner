3536e36875e7861695ee0e4ab9230d53f7d897cf
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-17 12:35:50 +0430

    Implementing test_sign_user_in

--- planner/tests/test_login.py
+++ planner/tests/test_login.py
@@ -16,7 +16,7 @@ async def test_sign_new_user(default_client: httpx.AsyncClient) -> None:
     }
     headers = {
         "accept": "application/json",
-        "Content-type": "application/json"
+        "Content-Type": "application/json"
     }
     test_response = {
         "message": "User created successfully"
@@ -24,3 +24,18 @@ async def test_sign_new_user(default_client: httpx.AsyncClient) -> None:
     response = await default_client.post("/user/signup", json=payload, headers=headers)
     assert response.status_code == 200
     assert response.json() == test_response
+
+
+@pytest.mark.asyncio
+async def test_sign_user_in(default_client: httpx.AsyncClient) -> None:
+    payload = {
+        "username": "testuser@test.com",
+        "password": "testpassword"
+    }
+    headers = {
+        "accept": "application/json",
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    response = await default_client.post("/user/signin", data=payload, headers=headers)
+    assert response.status_code == 200
+    assert response.json()["token_type"] == "Bearer"
